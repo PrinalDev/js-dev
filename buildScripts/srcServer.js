d7144e07865220e5a7f39a70027eb1c70fbf9386
Prince Alexandre d'ALMEIDA <prinaldev@gmail.com>
2020-07-04 14:06:46 +0100

    https call with mock api

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -21,6 +21,24 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../src/index.html"));
 });
 
+app.get("/users", function(req, res) {
+    res.json([
+        { id: 1, firstName: "Bob", lastName: "Smith", email: "bob@gmail.com" },
+        {
+            id: 2,
+            firstName: "Marcelline",
+            lastName: "ADJAKOU",
+            email: "marcelline@yahoo.com",
+        },
+        {
+            id: 3,
+            firstName: "Prinal",
+            lastName: "d'ALMEIDA",
+            email: "prinaldev@gmail.com",
+        },
+    ]);
+});
+
 app.listen(port, function(err) {
     if (err) {
         console.log(err);
